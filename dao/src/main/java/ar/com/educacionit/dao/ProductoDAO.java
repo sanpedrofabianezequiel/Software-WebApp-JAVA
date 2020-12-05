4d49c90379308b92f7d9534b293c3454b666658e
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-12-05 19:12:55 -0300

    Dependencias

---
+++ dao/src/main/java/ar/com/educacionit/dao/ProductoDAO.java
@@ -0,0 +1,15 @@
+package ar.com.educacionit.dao;
+
+import ar.com.educacionit.domain.Producto;
+
+/*
+ * 
+ * Aquie vamos a tener el ABM - CRUD
+ * 
+ * 
+ * */
+public interface ProductoDAO {
+
+	public Producto create(Producto producto);
+	
+}
--- domain/src/main/java/ar/com/educacionit/domain/Producto.java
+++ domain/src/main/java/ar/com/educacionit/domain/Producto.java
@@ -6,5 +6,52 @@ package ar.com.educacionit.domain;
  * 
  * */
 public class Producto {
-
+	private Long id;
+	private String descripcion;
+	private Float precio;
+	private String codigo;
+	
+	//Constructores en el caso que venga con ID  o sin ID
+	public Producto(Long id, String descripcion, Float precio, String codigo) {
+		super();
+		this.id = id;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.codigo = codigo;
+	}
+	public Producto(String descripcion, Float precio, String codigo) {
+		super();
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.codigo = codigo;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Float precio) {
+		this.precio = precio;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	
+	
+	
+	
 }
