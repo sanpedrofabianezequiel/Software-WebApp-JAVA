123a9c2397374859e8ec31e06ae59b2371988152
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-12-16 23:19:46 -0300

    Iterator

--- WebApp/src/main/java/ar/com/educacionit/webapp/collections/ListaMain.java
+++ WebApp/src/main/java/ar/com/educacionit/webapp/collections/ListaMain.java
@@ -2,6 +2,7 @@ package ar.com.educacionit.webapp.collections;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 import ar.com.educacionit.domain.Producto;
 
@@ -14,6 +15,10 @@ public class ListaMain {
 		obj.add(item1);
 		//Eliminar en un ForEcah
 		//Para eliminar utilizamos la interfaz Iterator
+		//Determina el proximo elemento => Next()
+		//Determina si tiene productos siguientes => hasNex()
+		
+		Iterator<Producto> delete = obj.iterator();
 	}
 
 }
